3203d31ca8c77239af67936f599373f68d146c3b
Shuvojit <s15sarkar@yahoo.com>
2023-04-02 22:56:42 +0530

    created req handler

--- server/server.go
+++ server/server.go
@@ -1,16 +1,39 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func testApi(c *fiber.Ctx) error {
-    params := string(c.Request().URI().QueryString())
-	c.Status(200).SendString(params)
+type Request struct {
+    RequestTopic string `json:"requestTopic"`
+    ResponseTopic string `json:"responseTopic"`
+    Payload string `json:"payload"`
+}
+
+func handle(c *fiber.Ctx) error {
+    request := Request{}
+    if err := json.Unmarshal(c.Body(), &request); err != nil {
+        c.Status(400).JSON(map[string]string{})
+        return errors.New("Invalid request body")
+    }
+    log.Printf("Request: %v", request)
+    c.Status(200).JSON(map[string]string{
+        "message": "fetched successfully",
+    })
     return nil
 }
+
+func setupRoutes(app *fiber.App) {
+    api := app.Group("/v1")
+    api.Post("/", handle)
+}
+
 func Start() {
 	app := fiber.New()
-	app.Get("/", testApi)
+    setupRoutes(app)
 	app.Listen(":3000")
 }
